29110e4b5cde43c9cdff9cdcc49675066506192a
David <dcg29908@email.vccs.edu>
2025-02-11 14:37:48 -0500

    done

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                alerts: resolve(__dirname, "alerts.html"),
                 profile: resolve(__dirname, "profile.html"),
                 simple: resolve(__dirname, "simpleSite.html"),
                 color: resolve(__dirname, "colorGenerator.html")
